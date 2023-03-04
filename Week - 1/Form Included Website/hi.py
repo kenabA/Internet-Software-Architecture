be8defd67c8ff263f7423343d4a489466d672eb1
Kenab KC <kebab.bahadur@gmail.com>
2023-03-04 21:29:49 +0545

    Initial commit

---
+++ Week - 1/Form Included Website/hi.py
@@ -0,0 +1,28 @@
+def s1(n):
+  k = 0
+  for i in range (1,n+1):
+    k = k + i
+  return k 
+hi = s1(20)
+print(hi)
+
+# def s2(n):
+#  k = 0 
+#  for i in range( 1,n+1):
+#    k = k + i**2
+#  return k
+
+
+
+# def s3():
+#  main_answer = (3*s2(n))/s1(n)
+#  return main_answer 
+
+# final_answer = s3()
+
+# n = 20
+# for i in range (1, n+1):
+#   print(s1(i),s2(i))
+
+
+  
